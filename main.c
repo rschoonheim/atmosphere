a840507c50f3bdbc583f4968e923121f10f241c6
Romano Schoonheim <romano@consumind.nl>
2023-01-24 13:47:03 +0100

    Comment on main.c

--- main.c
+++ main.c
@@ -1,4 +1,14 @@
+/**
+ * Atmosphere Programming Language
+ * -------------------------------
+ * Atmosphere is a programming language designed to
+ * automate the process of software engineering.
+ *
+ * @author: Romano Schoonheim <support@atmosphere.sh>
+ */
+
 #include <stdio.h>
+
 
 int main() {
     printf("Hello, World!\n");
